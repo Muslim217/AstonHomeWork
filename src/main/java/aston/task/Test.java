d1fb46bcf03b0d4b3766999fe2c906b0a100414c
Muslim217 <abumuslimsafaraliev3815@gmail.com>
2024-03-07 17:40:54 +0300

    SecondCommit

--- src/main/java/aston/task/Test.java
+++ src/main/java/aston/task/Test.java
@@ -1,11 +1,26 @@
 package aston.task;
 
-import aston.task.list.ArrayListImp;
+
+import aston.task.list.ArrayList;
+import aston.task.list.LinkedList;
 import aston.task.list.List;
 
 
 public class Test {
     public static void main(String[] args) {
+        LinkedList<Integer> integerLinkedList = new LinkedList<>();
+        integerLinkedList.addFirst(1);
+        integerLinkedList.addLast(2);
+        integerLinkedList.addLast(3);
+        integerLinkedList.addLast(4);
+        integerLinkedList.addLast(5);
+        System.out.println(integerLinkedList);
+        System.out.println(integerLinkedList.getFirst());
+        System.out.println(integerLinkedList.getLast());
+        integerLinkedList.remove(4);
+        System.out.println(integerLinkedList);
+
+
 
     }
 }
--- src/main/java/aston/task/list/List.java
+++ src/main/java/aston/task/list/List.java
@@ -1,15 +1,23 @@
 package aston.task.list;
 
+import java.util.Comparator;
+
 public interface List<E> {
 
     void add(E e);
+
+    void addToIndex(E e,int index);
 
     E get(int index);
 
     boolean remove(int index);
 
+    boolean removeAll();
+
     int size();
 
     boolean isEmpty();
+
+    void sort(Comparator<E> comparator);
 
 }
